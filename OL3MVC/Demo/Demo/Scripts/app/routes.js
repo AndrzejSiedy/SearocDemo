1031b0a1717a6facfb8c0335bb7388fb1e01e114
wilk666 <andre@cartomatic.pl>
2014-10-03 11:40:34 +0200

    Add template for sammy router

---
+++ OL3MVC/Demo/Demo/Scripts/app/routes.js
@@ -0,0 +1,58 @@
+﻿(function ($) {
+
+    var app_initiated = false;
+
+    var app = $.sammy('#main', function () {
+
+        this.get("#/welcome", function (context) {
+            $(".view").hide();
+            $("#welcome-view").show("slide", { direction: "left" }, 500);
+        });
+
+        this.get("#/app", function (context) {
+            $(".view").hide();
+
+            // NOTE: for main app we cannot use "slide" effects as it will re-render whole app
+            // will load scripts etc.
+            $("#app-view").show(500, function () {
+                // Animation complete.
+                // Cannot start app when DOM is not read/visible on init
+                // Hence init app after first time app-view gets rendered
+                if (!app_initiated) {
+                    Gnx.init();
+                    Gnx.appStart();
+
+                    app_initiated = true;
+                }
+                else {
+                    // browser size could change - resize them all
+                    Gnx.Layout.viewport.resizeAll();
+                }
+
+            });
+        });
+
+        this.get("#/about", function (context) {
+            $(".view").hide();
+            $("#about-view").show("slide", { direction: "left" }, 500);
+        });
+
+        this.get("#/contact", function (context) {
+            $(".view").hide();
+            $("#contact-view").show("slide", { direction: "left" }, 500);
+        });
+
+
+        this.get("#/module", function (context) {
+            $(".view").hide();
+            $("#module-view").show("slide", { direction: "left" }, 500);
+        });
+
+    });
+
+    $(function () {
+        //$("#app-view").hide();
+        app.run('#/welcome');
+    });
+
+})(jQuery);
