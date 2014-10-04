0d7649839edc0c107fbce0984aa732ad92dc2c78
wilk666 <andre@cartomatic.pl>
2014-10-04 17:43:25 +0200

    Update demo show options
    seems to not make any difference

--- OL3MVC/Demo/Demo/Scripts/app/routes.js
+++ OL3MVC/Demo/Demo/Scripts/app/routes.js
@@ -14,14 +14,14 @@
 
             // NOTE: for main app we cannot use "slide" effects as it will re-render whole app
             // will load scripts etc.
-            $("#demo-view").show(500, function () {
+            $("#demo-view").show("slide", { direction: "left" }, 500, function () {
                 // Animation complete.
                 // Cannot start app when DOM is not read/visible on init
                 // Hence init app after first time app-view gets rendered
                 if (!app_initiated) {
                     Gnx.init();
                     Gnx.appStart();
-                    //app_initiated = true;
+                    app_initiated = true;
                 }
                 else {
                     // browser size could change - resize them all
